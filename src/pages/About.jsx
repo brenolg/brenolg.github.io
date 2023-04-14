26f804007d781b5577809666a00042ee5697f2a0
brenolg <brenolg@yahoo.com.br>
2023-04-14 08:53:44 -0300

    refactor: tirando tech Tilt das pages e retornando framer motion animation

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,7 @@
-import Tilt from 'react-parallax-tilt';
-import { motion } from 'framer-motion';
 import { useLocation } from 'react-router-dom';
 import React, { useEffect } from 'react';
+import { motion } from 'framer-motion';
 import styles from './About.module.css';
-import tiltProps from '../utils/tiltProps';
 import myImg from '../images/brenoSobre.png';
 import '../global.css';
 import motionProps from '../utils/motionProps';
@@ -21,57 +19,48 @@ export default function About() {
 
       <main>
         <section className={`${styles.img_section}`}>
-          <motion.div className="ball" id={styles.ball1} {...motionProps.ballMove} />
-          <motion.div className="ball" id={styles.ball2} {...motionProps.ballMove} />
-          <motion.div className="ball" id={styles.ball3} {...motionProps.ballMove} />
+          <motion.div className="ball" id={styles.ball1} {...motionProps.ball} />
+          <motion.div className="ball" id={styles.ball2} {...motionProps.ball} />
+          <motion.div className="ball" id={styles.ball3} {...motionProps.ball} />
 
-          <Tilt {...tiltProps.myImgProps}>
-            <motion.div className={`${styles.img_container} glass`} {...motionProps.mainContent}>
-              <img className={`${styles.my_img} glass`} src={myImg} alt="foto breno" />
-            </motion.div>
-          </Tilt>
+          <motion.div className={`${styles.img_container} glass`} {...motionProps.mainContent}>
+            <img className={`${styles.my_img} glass`} src={myImg} alt="foto breno" />
+          </motion.div>
 
-          <Tilt {...tiltProps.defaultProps}>
-            <motion.div className={`${styles.main_description} glass`} {...motionProps.mainContent}>
-              <h2 className={`${styles.titles}`}>Vida Profissional</h2>
-              <p className={`${styles.p}`}>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis commodi
-                numquam quod ea pariatur libero totam eius, id vero repellat neque ab, dignissimos
-                nobis nam quasi dolore, repudiandae delectus hic! Lorem ipsum dolor sit amet
-                consectetur, adipisicing elit. Facilis, nulla! Nihil, temporibus! Fuga at,
-                voluptatum deserunt, dolores voluptates tempore necessitatibus corrupti, temporibus
-                rerum sapiente laudantium odio itaque! Nobis, quidem neque. Lorem ipsum dolor sit
-                amet consectetur adipisicing elit. Excepturi quis exercitationem ipsa nam explicabo
-                quaerat, voluptatem alias! Libero dolorum quia molestiae qui ducimus perferendis
-                labore possimus, ad non, voluptatem nesciunt?
-              </p>
-            </motion.div>
-          </Tilt>
+          <motion.div className={`${styles.main_description} glass`} {...motionProps.mainContent}>
+            <h2 className={`${styles.titles}`}>Vida Profissional</h2>
+            <p className={`${styles.p}`}>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis commodi numquam
+              quod ea pariatur libero totam eius, id vero repellat neque ab, dignissimos nobis nam
+              quasi dolore, repudiandae delectus hic! Lorem ipsum dolor sit amet consectetur,
+              adipisicing elit. Facilis, nulla! Nihil, temporibus! Fuga at, voluptatum deserunt,
+              dolores voluptates tempore necessitatibus corrupti, temporibus rerum sapiente
+              laudantium odio itaque! Nobis, quidem neque. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Excepturi quis exercitationem ipsa nam explicabo quaerat, voluptatem
+              alias! Libero dolorum quia molestiae qui ducimus perferendis labore possimus, ad non,
+              voluptatem nesciunt?
+            </p>
+          </motion.div>
         </section>
 
         <section className={`${styles.relativSection}`}>
-          <motion.div className="ball" id={styles.ball4} {...motionProps.ballMove} />
-          <motion.div className="ball" id={styles.ball5} {...motionProps.ballMove} />
+          <motion.div className="ball" id={styles.ball4} {...motionProps.ball} />
+          <motion.div className="ball" id={styles.ball5} {...motionProps.ball} />
 
-          <Tilt {...tiltProps.defaultProps}>
-            <motion.div
-              className={`${styles.professional_description} glass`}
-              {...motionProps.mainContent}
-            >
-              <h2 className={`${styles.titles}`}>Curiosidades...</h2>
-              <p className={`${styles.p} `}>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis commodi
-                numquam quod ea pariatur libero totam eius, id vero repellat neque ab, dignissimos
-                nobis nam quasi dolore, repudiandae delectus hic! Lorem ipsum dolor sit amet
-                consectetur, adipisicing elit. Facilis, nulla! Nihil, temporibus! Fuga at,
-                voluptatum deserunt, dolores voluptates tempore necessitatibus corrupti, temporibus
-                rerum sapiente laudantium odio itaque! Nobis, quidem neque. Lorem ipsum dolor sit
-                amet consectetur adipisicing elit. Excepturi quis exercitationem ipsa nam explicabo
-                quaerat, voluptatem alias! Libero dolorum quia molestiae qui ducimus perferendis
-                labore possimus, ad non, voluptatem nesciunt?
-              </p>
-            </motion.div>
-          </Tilt>
+          <div className={`${styles.professional_description} glass`}>
+            <h2 className={`${styles.titles}`}>Curiosidades...</h2>
+            <p className={`${styles.p} `}>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis commodi numquam
+              quod ea pariatur libero totam eius, id vero repellat neque ab, dignissimos nobis nam
+              quasi dolore, repudiandae delectus hic! Lorem ipsum dolor sit amet consectetur,
+              adipisicing elit. Facilis, nulla! Nihil, temporibus! Fuga at, voluptatum deserunt,
+              dolores voluptates tempore necessitatibus corrupti, temporibus rerum sapiente
+              laudantium odio itaque! Nobis, quidem neque. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Excepturi quis exercitationem ipsa nam explicabo quaerat, voluptatem
+              alias! Libero dolorum quia molestiae qui ducimus perferendis labore possimus, ad non,
+              voluptatem nesciunt?
+            </p>
+          </div>
         </section>
       </main>
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect } from 'react';
-import Tilt from 'react-parallax-tilt';
 import { useLocation } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import Techs from '../components/Techs';
 import fotoBreno from '../images/fotoBreno.png';
 import StarWars from '../images/star-wars.gif';
 import styles from './Home.module.css';
-import tiltProps from '../utils/tiltProps';
 import '../global.css';
 import motionProps from '../utils/motionProps';
 
@@ -18,65 +17,58 @@ export default function Home() {
 
   return (
     <>
-      <div className="ball asideContent" id={styles.ballMove} />
+      <div className="ball " id={styles.ballMove} />
 
       <main>
         <div className={`${styles.profile_container}`}>
-          <div className="ball asideContent" id={styles.ball1} {...motionProps.ballMove} />
-          <div className="ball asideContent" id={styles.ball2} {...motionProps.ballMove} />
-          <div className="ball asideContent" id={styles.ball3} {...motionProps.ballMove} />
+          <motion.div className="ball" id={styles.ball1} {...motionProps.ball} />
+          <motion.div className="ball" id={styles.ball2} {...motionProps.ball} />
+          <motion.div className="ball" id={styles.ball3} {...motionProps.ball} />
 
-          <Tilt {...tiltProps.defaultProps}>
-            <div className={`${styles.profile} glass mainContent`}>
-              <div className={styles.img_border}>
-                <img className={styles.my_img} src={fotoBreno} alt="Foto Breno Lavalle Garrido" />
-              </div>
-              <div className={styles.text}>
-                <h2>Breno Lavalle Garrido</h2>
-                <span>Desenvolvedor Web Full-Stack </span>
+          <motion.div className={`${styles.profile} glass`} {...motionProps.mainContent}>
+            <div className={styles.img_border}>
+              <img className={styles.my_img} src={fotoBreno} alt="Foto Breno Lavalle Garrido" />
+            </div>
+            <div className={styles.text}>
+              <h2>Breno Lavalle Garrido</h2>
+              <span>Desenvolvedor Web Full-Stack </span>
 
-                <p>
-                  Desenvolvedor com experiência em React, Redux, JavaScript, React
-                  Hooks,Context-api, Jest, React testing library, Docker, Node.js, MySQL, TypeScript
-                  e consumo de APIs REST. Curioso e fascinado pelos desafios lógicos que a
-                  programação promove. Também possui experiência em projetos gerenciados por
-                  Metodologias Ágeis. Estudante do curso Web Full-Stack da Trybe e cursando no
-                  momento backend.
-                </p>
-                <div className={styles.profileBtns}>
-                  <button className={styles.btn} type="button">
-                    Entrar em Contato
-                  </button>
+              <p>
+                Desenvolvedor com experiência em React, Redux, JavaScript, React Hooks,Context-api,
+                Jest, React testing library, Docker, Node.js, MySQL, TypeScript e consumo de APIs
+                REST. Curioso e fascinado pelos desafios lógicos que a programação promove. Também
+                possui experiência em projetos gerenciados por Metodologias Ágeis. Estudante do
+                curso Web Full-Stack da Trybe e cursando no momento backend.
+              </p>
+              <div className={styles.profileBtns}>
+                <button className={styles.btn} type="button">
+                  Entrar em Contato
+                </button>
 
-                  <button className={styles.btn} type="button">
-                    Download CV
-                  </button>
-                </div>
+                <button className={styles.btn} type="button">
+                  Download CV
+                </button>
               </div>
             </div>
-          </Tilt>
+          </motion.div>
         </div>
 
         <section className={styles.hard_skills}>
-          <div className="ball asideContent" id={styles.ball4} />
-          <div className="ball asideContent" id={styles.ball5} />
-          <div className="ball asideContent" id={styles.ball6} />
-          <div className="ball asideContent" id={styles.ball7} />
+          <motion.div className="ball" id={styles.ball4} {...motionProps.ball} />
+          <motion.div className="ball" id={styles.ball5} {...motionProps.ball} />
+          <motion.div className="ball" id={styles.ball6} {...motionProps.ball} />
+          <motion.div className="ball" id={styles.ball7} {...motionProps.ball} />
 
-          <Tilt {...tiltProps.defaultProps}>
-            <div className={`${styles.repos_preview} glass mainContent`} {...motionProps.ballMove}>
-              <img src={StarWars} alt="Projects Gifts" />
-              <button className={styles.btn} type="button">
-                Projects Page
-              </button>
-            </div>
-          </Tilt>
+          <motion.div className={`${styles.repos_preview} glass`} {...motionProps.mainContent}>
+            <img src={StarWars} alt="Projects Gifts" />
+            <button className={styles.btn} type="button">
+              Projects Page
+            </button>
+          </motion.div>
 
-          <Tilt {...tiltProps.techContainerProps}>
-            <div className={` glass mainContent${styles.stacks_container}`}>
-              <Techs />
-            </div>
-          </Tilt>
+          <motion.div className={`${styles.stacks_container} glass`} {...motionProps.ball}>
+            <Techs />
+          </motion.div>
         </section>
       </main>
     </>
